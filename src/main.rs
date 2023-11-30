2cdb916b995b5e5ec6945851bdfa0abe6be2e3f3
Frytak <frytak2855@gmail.com>
2023-11-30 22:13:34 +0100

    List raw input devices

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::{ptr::null_mut, mem::{size_of, self}, ffi::c_void};
 
-use windows::Win32::{UI::{WindowsAndMessaging::{SetWindowsHookExA, WH_MOUSE, WH_KEYBOARD, WH_MOUSE_LL, GetMessageA, WINDOWS_HOOK_ID}, Input::{KeyboardAndMouse::{INPUT, INPUT_0, MOUSEINPUT, INPUT_TYPE, MOUSE_EVENT_FLAGS, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, SendInput}, RAWINPUTDEVICE, RIDEV_NOLEGACY, RegisterRawInputDevices, GetRawInputDeviceList, RAWINPUTDEVICELIST, GetRawInputDeviceInfoA, RIDI_DEVICEINFO, RID_DEVICE_INFO, RID_DEVICE_INFO_TYPE, RID_DEVICE_INFO_0}}, Foundation::{WPARAM, LRESULT, LPARAM, HINSTANCE, HWND, GetLastError}, Devices::HumanInterfaceDevice::{HID_USAGE_PAGE_GENERIC, HID_USAGE_GENERIC_KEYBOARD}};
+use windows::Win32::{UI::{WindowsAndMessaging::{SetWindowsHookExA, WH_MOUSE, WH_KEYBOARD, WH_MOUSE_LL, GetMessageA, WINDOWS_HOOK_ID}, Input::{KeyboardAndMouse::{INPUT, INPUT_0, MOUSEINPUT, INPUT_TYPE, MOUSE_EVENT_FLAGS, MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, SendInput}, RAWINPUTDEVICE, RIDEV_NOLEGACY, RegisterRawInputDevices, GetRawInputDeviceList, RAWINPUTDEVICELIST, GetRawInputDeviceInfoA, RIDI_DEVICEINFO, RID_DEVICE_INFO, RID_DEVICE_INFO_TYPE, RID_DEVICE_INFO_0, RIDEV_REMOVE}}, Foundation::{WPARAM, LRESULT, LPARAM, HINSTANCE, HWND, GetLastError}, Devices::HumanInterfaceDevice::{HID_USAGE_PAGE_GENERIC, HID_USAGE_GENERIC_KEYBOARD, HID_USAGE_GENERIC_MOUSE}};
 
-const RAWINPUTDEVICES: [RAWINPUTDEVICE; 1] = [RAWINPUTDEVICE {usUsagePage: HID_USAGE_PAGE_GENERIC, usUsage: HID_USAGE_GENERIC_KEYBOARD, dwFlags: RIDEV_NOLEGACY, hwndTarget: HWND(0)}];
+/// Array of input devices to register
+///
+/// This one only registers keyboards
+const RAWINPUTDEVICES: [RAWINPUTDEVICE; 1] = [
+    RAWINPUTDEVICE {
+        usUsagePage: HID_USAGE_PAGE_GENERIC,
+        usUsage: HID_USAGE_GENERIC_KEYBOARD,
+        dwFlags: RIDEV_NOLEGACY,
+        hwndTarget: HWND(0)
+    }
+];
+
 const MOUSE_CLICK: INPUT = INPUT {
     r#type: INPUT_TYPE(0),
     Anonymous: INPUT_0 {
@@ -17,32 +28,94 @@ const MOUSE_CLICK: INPUT = INPUT {
     }
 };
 
-// unsafe fn callback(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {}
-unsafe extern "system" fn callback(code: i32, param: WPARAM, lpdata: LPARAM) -> LRESULT {
-    println!("F");
-    //CallNextHookEx(, code, param, lpdata)
-    LRESULT(0)
-}
+fn main() -> Result<(), String> {
+    let mut device_list: Vec<RAWINPUTDEVICELIST>;
+    let mut device_count: u32 = 0;
 
-fn main() {
     unsafe {
-        let mut device_list = [RAWINPUTDEVICELIST::default(); 8];
-        let mut device_count: u32 = 0;
-        let mut device_info = RID_DEVICE_INFO::default();
-        let mut device_info_size: u32 = size_of::<RID_DEVICE_INFO>() as u32;
-        let device_info_ptr: *mut c_void = (&mut device_info as *mut _) as *mut c_void;
+        RegisterRawInputDevices(&RAWINPUTDEVICES, size_of::<RAWINPUTDEVICE>() as u32).map_err(|err| {
+            format!("Couldn't register for raw input. {:?}", err)
+        })?;
 
-        device_info.cbSize = size_of::<RID_DEVICE_INFO>() as u32;
-        dbg!(device_info.cbSize, size_of::<RID_DEVICE_INFO>());
+        // Get the amount of raw input devices and allocate an apropariate space
+        if GetRawInputDeviceList(None, &mut device_count, size_of::<RAWINPUTDEVICELIST>() as u32) == u32::MAX {
+            return Err(format!("Couldn't get the amount of raw input devices. {:?}", GetLastError().expect_err("No error found while getting the amount of raw input devices.")));
+        }
 
-        RegisterRawInputDevices(&RAWINPUTDEVICES, size_of::<RAWINPUTDEVICE>() as u32).unwrap();
-        GetRawInputDeviceList(None, &mut device_count, size_of::<RAWINPUTDEVICELIST>() as u32);
-        // Alloc device_count
-        GetRawInputDeviceList(Some(&mut device_list[0]), &mut device_count, size_of::<RAWINPUTDEVICELIST>() as u32);
-        dbg!(device_count, device_list);
-        dbg!(GetRawInputDeviceInfoA(device_list[0].hDevice, RIDI_DEVICEINFO, Some(device_info_ptr), &mut device_info_size));
-        dbg!(device_list[0], device_info.dwType);
-        //SetWindowsHookExA(WINDOWS_HOOK_ID(13), Some(callback), HINSTANCE::default(), 0).unwrap();
+        device_list = vec![RAWINPUTDEVICELIST::default(); device_count as usize];
+
+        // Assign raw input devices into the array
+        if GetRawInputDeviceList(Some(&mut device_list[0]), &mut device_count, size_of::<RAWINPUTDEVICELIST>() as u32) == u32::MAX {
+            return Err(format!("Couldn't get the list of raw input devices. {:?}", GetLastError().expect_err("No error found while getting the list of raw input devices.")));
+        }
+    }
+
+    println!("====================================");
+    println!("     Raw input devices status       ");
+    println!("====================================");
+    println!("Device count: {}", device_count);
+    println!("Device list:");
+    for (i, device) in device_list.iter().enumerate() {
+        println!("\t{}: Handle({}), Type({})", i, device.hDevice.0, device.dwType.0);
+    }
+
+
+
+    for device in device_list {
+        let mut device_info_size: u32 = 0;
+        let mut device_info = RID_DEVICE_INFO {
+            cbSize: size_of::<RID_DEVICE_INFO>() as u32,
+            dwType: RID_DEVICE_INFO_TYPE::default(),
+            Anonymous: RID_DEVICE_INFO_0::default()
+        };
+
+        unsafe {
+            let device_info_ptr: *mut c_void = (&mut device_info as *mut _) as *mut c_void;
+
+            // Get required size... For no reason as I use RIDI_DEVICEINFO, I already know it but
+            // Windows still requires it! Or else it errors that it doesn't have enough space for the
+            // data even though it does.
+            if GetRawInputDeviceInfoA(device.hDevice, RIDI_DEVICEINFO, None, &mut device_info_size) == u32::MAX {
+                return Err(format!("Couldn't get required size for device with handle {}. {:?}", device.hDevice.0, GetLastError().expect_err("No error found while getting required size for device.")));
+            }
+
+            if GetRawInputDeviceInfoA(device.hDevice, RIDI_DEVICEINFO, Some(device_info_ptr), &mut device_info_size) == u32::MAX {
+                return Err(format!("Couldn't get more information on device with handle {}. {:?}", device.hDevice.0, GetLastError().expect_err("No error found while getting more information on device.")));
+            }
+        }
+
+        println!("====================================");
+        println!("      Raw input devices info        ");
+        println!("====================================");
+        println!("Device handle: {}", device.hDevice.0);
+
+        match device_info.dwType {
+            RID_DEVICE_INFO_TYPE(0) => unsafe {
+                println!("\tId: {}", device_info.Anonymous.mouse.dwId);
+                println!("\tNumber of buttons: {}", device_info.Anonymous.mouse.dwNumberOfButtons);
+                println!("\tSample rate: {}", device_info.Anonymous.mouse.dwSampleRate);
+                println!("\tHas horizontal wheel: {}", device_info.Anonymous.mouse.fHasHorizontalWheel.0);
+            },
+            RID_DEVICE_INFO_TYPE(1) => unsafe {
+                println!("\tType: {}", device_info.Anonymous.keyboard.dwType);
+                println!("\tSub-type: {}", device_info.Anonymous.keyboard.dwSubType);
+                println!("\tMode: {}", device_info.Anonymous.keyboard.dwKeyboardMode);
+                println!("\tNumber of function keys: {}", device_info.Anonymous.keyboard.dwNumberOfFunctionKeys);
+                println!("\tNumber of indicators: {}", device_info.Anonymous.keyboard.dwNumberOfIndicators);
+                println!("\tNumber of total keys: {}", device_info.Anonymous.keyboard.dwNumberOfKeysTotal);
+            },
+            RID_DEVICE_INFO_TYPE(2) => unsafe {
+                println!("\tVendor ID: {}", device_info.Anonymous.hid.dwVendorId);
+                println!("\tProduct ID: {}", device_info.Anonymous.hid.dwProductId);
+                println!("\tVersion: {}", device_info.Anonymous.hid.dwVersionNumber);
+                println!("\tUsage page: {}", device_info.Anonymous.hid.usUsagePage);
+                println!("\tUsage: {}", device_info.Anonymous.hid.usUsage);
+            },
+            _ => { panic!("Impossible or yet unimplemented type of device."); }
+        }
+    }
+
+    unsafe {
         GetMessageA(null_mut(), HWND::default(), 0, 0);
     }
 
@@ -57,4 +130,6 @@ fn main() {
             println!("{:?}", error);
         }
     }
+
+    Ok(())
 }
